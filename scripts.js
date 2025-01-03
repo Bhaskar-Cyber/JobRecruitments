d989b44aa4ece1f8903a973d3abe1c6353a26dc5
Vadai Bhaskar <vadaibhaskar420@gmail.com>
2025-01-03 23:32:06 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -47,3 +47,25 @@
  
      // Insert the new job card at the beginning
      jobListings.insertBefore(newJobCard, jobListings.firstChild);
+
+
+
+
+    // Create a new job card element
+     var newJobCard = document.createElement('div');
+     newJobCard.classList.add('job-card'); // Add the job-card class
+ 
+     // Add content to the new job card
+     newJobCard.innerHTML = `
+         <h3>Python Full Stack Developer</h3>
+         <p>Company Name: Accenture</p>
+         <p>Location: Hyderabad</p>
+         <p>Experience: Fresher</p>
+        <a href="FindYourJobTodayAboutJobDetails.html"><button>View Details</button></a>
+     `;
+ 
+     // Get the job listings container
+     var jobListings = document.querySelector('.job-listings');
+ 
+     // Insert the new job card at the beginning
+     jobListings.insertBefore(newJobCard, jobListings.firstChild);
